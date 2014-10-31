2e84f384ec84d4b952753bd6a71b3f54da31c7c4
Miguel Dias Gomes <mdg@uninova.pt>
2014-10-31 14:41:45 +0000

    * Fixed build scripts to build stuff to dist * Removed cos.jar * Added Junit 4 lib (+ hamcrest dep), catalina.jar, servlet-api.jar (from Java EE 7/8) and PostgreSQL and SQLite jdbc driver libs * Updated the UWSUrlTest case for EE7 servelet
    Signed-off-by: Miguel Dias Gomes <mdg@uninova.pt>

--- test/uws/service/UWSUrlTest.java
+++ test/uws/service/UWSUrlTest.java
@@ -25,6 +25,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpUpgradeHandler;
 import javax.servlet.http.Part;
 
 import org.junit.Before;
@@ -379,6 +380,25 @@ public class UWSUrlTest {
 
 		@Override
 		public void logout() throws ServletException{}
+
+		@Override
+		public long getContentLengthLong() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public String changeSessionId() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public <T extends HttpUpgradeHandler> T upgrade(Class<T> arg0)
+				throws IOException, ServletException {
+			// TODO Auto-generated method stub
+			return null;
+		}
 
 	}
 
