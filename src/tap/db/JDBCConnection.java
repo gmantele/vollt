gmantele/vollt

commit 09ee79c485dab82e5f9fc67a85d1ae4a0139d421

[TAP] Fix the loading of columns when the column coosys_id exists.

src/tap/db/JDBCConnection.java
@@ -201,6 +201,11 @@ public class JDBCConnection implements DBConnection {
 	/** Name of the database column giving the database name of a TAP column, table or schema. */
 	protected final static String DB_NAME_COLUMN = "dbname";
 
+	/** Name of the database column giving the coordinate system ID associated
+	 * with a TAP column.
+	 * @since 2.1 */
+	protected final static String COOSYS_ID_COLUMN = "dbname";
+
 	/** Connection ID (typically, the job ID). It lets identify the DB errors linked to the Job execution in the logs. */
 	protected final String ID;
 
@@ -1302,8 +1307,10 @@ public class JDBCConnection implements DBConnection {
 				sqlBuf.append(", ");
 				translator.appendIdentifier(sqlBuf, DB_NAME_COLUMN, IdentifierField.COLUMN);
 			}
-			if (hasCoosys)
-				sqlBuf.append(", ").append(translator.getColumnName(tableDef.getColumn("coosys_id")));
+			if (hasCoosys){
+				sqlBuf.append(", ");
+				translator.appendIdentifier(sqlBuf, COOSYS_ID_COLUMN, IdentifierField.COLUMN);
+			}
 			sqlBuf.append(" FROM ").append(translator.getTableName(tableDef, supportsSchema));
 			if (hasColumnIndex)
 				sqlBuf.append(" ORDER BY 1,12,2");

commit 34d1859d33fc84b6d864c8afe4e9e52b200e9059

[TAP] Fix typo in the constant JDBCConnection.COOSYS_ID_COLUMN
This commit fixes a typo introduced in the commit
09ee79c485dab82e5f9fc67a85d1ae4a0139d421 ; because of that the VOTable's COOSYS
was not added anymore in query results.

src/tap/db/JDBCConnection.java
@@ -181,7 +181,7 @@ import uws.service.log.UWSLog.LogLevel;
  * </i></p>
  *
  * @author Gr&eacute;gory Mantelet (CDS;ARI)
- * @version 2.1 (01/2018)
+ * @version 2.1 (02/2018)
  * @since 2.0
  */
 public class JDBCConnection implements DBConnection {
@@ -198,13 +198,14 @@ public class JDBCConnection implements DBConnection {
 	/** DBMS name of Oracle used in the database URL. */
 	protected final static String DBMS_ORACLE = "oracle";
 
-	/** Name of the database column giving the database name of a TAP column, table or schema. */
+	/** Name of the database column giving the database name of a TAP column,
+	 * table or schema. */
 	protected final static String DB_NAME_COLUMN = "dbname";
 
 	/** Name of the database column giving the coordinate system ID associated
 	 * with a TAP column.
 	 * @since 2.1 */
-	protected final static String COOSYS_ID_COLUMN = "dbname";
+	protected final static String COOSYS_ID_COLUMN = "coosys_id";
 
 	/** Connection ID (typically, the job ID). It lets identify the DB errors linked to the Job execution in the logs. */
 	protected final String ID;